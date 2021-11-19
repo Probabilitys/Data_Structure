6b9ec20097b833c8c0f2031795ad6fd940701fcc
Probabilitys <91799472+Probabilitys@users.noreply.github.com>
2021-11-18 18:19:18 -0600

    Add files via upload

---
+++ IndexList.java
@@ -0,0 +1,29 @@
+/**
+ * IndexList (aka Vector) ADT
+ */
+
+public interface IndexList<E> {
+
+    /** Returns the number of elements in this list */
+    public int size();
+
+    /** Returns whether the list is empty */
+    public boolean isEmpty();
+
+    /** Inserts an element e at index i */
+    public void add(int i, E e)
+      throws IndexOutOfBoundsException;
+
+    /** Returns the element at index i without removing it */
+    public E get(int i)
+      throws IndexOutOfBoundsException;
+
+    /** Removes and returns the element at index i */
+    public E remove(int i)
+      throws IndexOutOfBoundsException;
+
+    /** Replaces the element at index i with e and returns the previous element */
+    public E set(int i, E e)
+      throws IndexOutOfBoundsException;
+
+}
