ee01e08d8f9709c1e89fc58324e42b82cb53cc69
Probabilitys <91799472+Probabilitys@users.noreply.github.com>
2021-11-30 23:30:22 -0600

    Update PositionalList.java

--- PositionalList.java
+++ PositionalList.java
@@ -6,44 +6,44 @@
 public interface PositionalList<E> {
     
     /** Returns the number of elements. */
-    int size();
+    public int size();
 
     /** Indicates whether the list is empty. */
-    boolean isEmpty();
+    public boolean isEmpty();
 
     /** Returns the first position, or null if empty. */
-    Position<E> first();
+    public Position<E> first();
 
     /** Returns the last position, or null if empty. */
-    Position<E> last();
+    public Position<E> last();
 
     /** Returns the Position immedidately before p, or null if p is first. */
-    Position<E> before(Position<E> p)
+    public Position<E> before(Position<E> p)
         throws IllegalArgumentException;
 
     /** Returns the Position immedidately after p, or null if p is last. */
-    Position<E> after(Position<E> p)
+    public Position<E> after(Position<E> p)
         throws IllegalArgumentException;
 
     /** Add element e to the front and returns its new position. */
-    Position<E> addFirst(E e);
+    public Position<E> addFirst(E e);
 
     /** Add element e to the end and returns its new position. */
-    Position<E> addLast(E e);
+    public Position<E> addLast(E e);
 
     /** Add element e immediately before p and returns its new position. */
-    Position<E> addBefore(Position<E> p, E e)
+    public Position<E> addBefore(Position<E> p, E e)
         throws IllegalArgumentException;
 
     /** Add element e immediately after p and returns its new position. */
-    Position<E> addAfter(Position<E> p, E e)
+    public Position<E> addAfter(Position<E> p, E e)
         throws IllegalArgumentException;
     
     /** Replaces the element at p and returns the old element. */
-    E set(Position<E> p, E e)
+    public E set(Position<E> p, E e)
         throws IllegalArgumentException;
 
     /** Remove the element at p and returns the removed element. */
-    E remove(Position<E> p)
+    public E remove(Position<E> p)
         throws IllegalArgumentException;
 }
