db73fd3286f32cbdd6c2a56c1c10b4ea5e588bb1
Probabilitys <91799472+Probabilitys@users.noreply.github.com>
2022-01-12 11:34:00 -0600

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -1,9 +1,9 @@
 /**
  * Sequence ADT
- * The Sequence ADT is the union of the Vector and List ADTs
+ * The Sequence ADT is the union of the Vector(IndexList) and List ADTs
  */
 
-public interface Sequence<E> extends Deque<E>, Vector<E>, PositionList<E> {
+public interface Sequence<E> extends Deque<E>, IndexList<E>, PositionList<E> {
 
     /** Returns the position at a given rank r. */
     public Position<E> atIndex(int r) throws BoundaryViolationException;
